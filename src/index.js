cd1d222dbb0bd921264bf12bfa011dd26dc15366
baokimho <kim.ho@edu.turkuamk.fi>
2025-07-26 01:23:08 +0300

    fix: ignore node_modules

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+app.use(express.static(path.join(__dirname,'resources/public')))
+app.use(morgan('combined'))
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home') 
+})
+app.get('/news', (req, res) => {
+  res.render('news') 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
