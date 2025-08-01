7d1fe26df160719ee534f30abba119b7d161c6eb
baokimho <kim.ho@edu.turkuamk.fi>
2025-08-01 05:35:07 +0300

    k

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const courseRouter = require('./courses');
 
 function route(app) {
     app.use('/', siteRouter);
     app.use('/news', newsRouter);
+    app.use('/courses', courseRouter);
 }
 
 module.exports = route;
